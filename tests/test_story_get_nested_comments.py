25dbe4c739e3f0b501d3c24016303b46da4e8cc0
ueg1990 <uehtesham90@gmail.com>
2014-03-18 18:22:13 -0400

    Added unittest using older Story

--- tests/test_story_get_nested_comments.py
+++ tests/test_story_get_nested_comments.py
@@ -3,13 +3,17 @@ import unittest
 
 class TestStoryGetComments(unittest.TestCase):
 
-    def setUp(self):
-        self.story = Story.fromid(7404389)
-        self.comments = self.story.get_comments()
-
     def test_get_nested_comments(self):
+    	self.story = Story.fromid(7404389)
+        self.comments = self.story.get_comments()
     	comment = self.comments[0].body
     	self.assertTrue(len(comment) >= 5508)
+
+    def test_get_nested_comments_old_story(self):
+    	self.story = Story.fromid(7410260)
+        self.comments = self.story.get_comments()
+    	comment = self.comments[0].body
+    	self.assertEqual(len(comment), 2131)
 
 if __name__ == '__main__':
      unittest.main()
