62d0be0f6077c5779a89e5ae85091c126b6af0cb
ueg1990 <uehtesham90@gmail.com>
2014-03-19 12:20:16 -0400

    Fixed indentation error

--- tests/test_story_get_nested_comments.py
+++ tests/test_story_get_nested_comments.py
@@ -6,8 +6,8 @@ class TestStoryGetComments(unittest.TestCase):
     def test_get_nested_comments(self):
     	self.story = Story.fromid(7404389)
         self.comments = self.story.get_comments()
-    	comment = self.comments[0].body
-    	self.assertTrue(len(comment) >= 5508)
+        comment = self.comments[0].body
+        self.assertTrue(len(comment) >= 5508)
 
     def test_get_nested_comments_old_story(self):
     	self.story = Story.fromid(7410260)
